88e4da4f0c976c0a8ed70640cd058b7ce6b0fc81
bouncei <joshuainyang255@gmail.com>
2022-05-12 02:03:04 +0100

    NFT and NFT Marketplace Smart Contracts Written in SOL

---
+++ contracts/NFTMarket.sol
@@ -0,0 +1,105 @@
+// SPDX-Lincense-Identifier: MIT
+
+
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+
+contract NFTMaket is ReentrancyGuard {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _itemIds;
+    Counters.Counter private _itemsSold;
+
+
+    address payable owner;
+    uint256 listingPrice = 0.025 ether;
+
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    struct MarketItem {
+        uint itemId;
+        address nftContract;
+        uint256 tokenId;
+        address payable seller;
+        address payable owner;
+        uint256 price;
+        bool sold;
+    }
+    mapping(uint256 => MarketItem) private idToMarketItem;
+
+
+    event MarketItemCreated (
+        uint indexed itemId ,
+        address indexed nftContract,
+        uint256 indexed tokenId,
+        address seller,
+        address owner,
+        uint256 price,
+        bool sold
+    );
+
+    function getListingPrice() public view returns (uint256) {
+        return listingPrice;
+    }
+
+    function creteMarketitem(
+        address nftContract,
+        uint256 tokenId,
+        uint256 price
+    ) public payable nonReentrant { 
+        require(price > 0, "Price must be least 1 wei");
+        require(msg.value == listingPrice, "Price must be equal to listing price");
+        
+        _itemIds.increment();
+        uint256 itemId = _itemIds.current();
+
+        idToMarketItem[itemId] = MarketItem(
+            itemId,
+            nftContract,
+            tokenId,
+            payable(msg.sender),
+            payable(address(0)),
+            price,
+            false
+        );
+
+        IERC721(nftContract).transferFrom(msg.sender, address(this), tokenId);
+        
+        emit MarketItemCreated(itemId, nftContract, tokenId, msg.sender, address(0), price, false);
+
+
+    }
+
+    function createMarketSale(
+        address nftContract,
+        uint256 itemId
+
+    ) public payable nonReentrant {
+        uint price = idToMarketItem[itemId].price;
+        uint tokenId = idToMarketItem[itemId].tokenId;
+
+        require(msg.value == price, "Please submit the asking price in order to complete the purchase");
+
+        idToMarketItem[itemId].seller.transfer(msg.value);
+        IERC721(nftContract).transferFrom(address(this), msg.sender, tokenId);
+        idToMarketItem[itemId].owner = payable(msg.sender);
+        idToMarketItem[itemId].sold = true;
+        _itemsSold.increment();
+        payable(owner).transfer(listingPrice);
+
+    }
+
+
+    
+
+
+}
+
+
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,23 @@
 require('@nomiclabs/hardhat-waffle')
+const fs = require('fs') // File Service
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-// task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-//   const accounts = await hre.ethers.getSigners();
+const privateKey = fs.readFileSync('.secret').toString()
+const projectId = 'b2d351e8f6b54a4daf84e1265fb2eb98'
 
-//   for (const account of accounts) {
-//     console.log(account.address);
-//   }
-// });
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+// Polygon Network Configurations
 module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: [privateKey],
+    },
+    mainet: {
+      url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
+      accounts: [privateKey],
+    },
+  },
   solidity: '0.8.4',
 }
