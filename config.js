32894e952c123fe68743df02d2869530ee17ba46
bouncei <joshuainyang255@gmail.com>
2022-05-15 23:21:34 +0100

    commit ...

--- config.js
+++ config.js
@@ -1,2 +1,6 @@
-export const nftaddress = '0x0165878A594ca255338adfa4d48449f69242Eb8F'
-export const nftmarketaddress = '0x5FC8d32690cc91D4c39d9d3abcBD16989F875707'
+export const nftaddress = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512'
+export const nftmarketaddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3'
+
+// 0x0165878A594ca255338adfa4d48449f69242Eb8F
+
+// 0x5FC8d32690cc91D4c39d9d3abcBD16989F875707  {marketplace}
--- pages/create.js
+++ pages/create.js
@@ -1,4 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { ethers } from 'hardhat'
+import { useRouter } from 'next/router'
+
+import Web3Modal from 'web3modal'
 
 function create() {
   return <div>create</div>
